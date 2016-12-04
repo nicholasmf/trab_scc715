e83da1c7c3a5ecae3e7f75a384e12a1da5c23e10
nicholasmf <nicholas_fujimoto@hotmail.com>
2016-12-04 09:30:05 -0200

    initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+var index = require('./routes/index');
+
+var app = express();
+
+app.set('views', path.join(__dirname), 'views');
+app.set('view engine', 'jade');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+
+var server = app.listen(8081, function() {
+	var host = server.address().address;
+	var port = server.address().port;
+	console.log("App listening at http://%s:%s", host, port);
+});
+
