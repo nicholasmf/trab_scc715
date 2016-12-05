6df623927e10830880be5f1e70e10d1e703e1dda
nicholasmf <nicholas_fujimoto@hotmail.com>
2016-12-05 17:15:15 -0200

    using localstorage

--- app.js
+++ app.js
@@ -2,16 +2,18 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
 var index = require('./routes/index');
+var add = require('./routes/add');
 
 var app = express();
 
-app.set('views', path.join(__dirname), 'views');
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
+app.use('/add', add);
 
 var server = app.listen(8081, function() {
 	var host = server.address().address;
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 
 router.get('/', function (req, res) {
-	res.render('/views/index.jade');
+	res.render('/index.jade');
 });
 
 module.exports = router;
